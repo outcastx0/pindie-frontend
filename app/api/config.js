f5a2511e0859c1e62e55a386f313a14d9552cf24
outcast <vx4749720@gmail.com>
2024-05-24 07:29:55 +0300

    cors

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://outcast.nomoredomainswork.ru/api'; //https://api-code-2.practicum-team.ru //http://localhost:3001/api //https://outcast.nomoredomainswork.ru/api
+export const BASE_URL = 'http://localhost:3001/api'; //https://api-code-2.practicum-team.ru //http://localhost:3001/api //https://outcast.nomoredomainswork.ru/api
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
